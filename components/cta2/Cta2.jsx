29730174e4e851de28c67b29c7acc7039277436d
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2024-09-19 16:23:08 +0530

    done

--- components/cta2/Cta2.jsx
+++ components/cta2/Cta2.jsx
@@ -31,13 +31,12 @@ function Cta2() {
                                                     Work Together</span>
                                             </div>
                                             <div class="pb-1 pt-20" data-aos="zoom-in-right" data-aos-duration="2000">
-                                                <a href="javascript:;"
-                                                    class="inline-flex items-center gap-3 md:text-lg 2xl:text-2xl  text-sm text-white relative ps-6 2xl:ps-8 3xl:ps-10 2xl:py-3  py-2 pr-6 transition-all duration-500 before:absolute before:border-2 before:border-white md:before:min-w-[42px] 2xl:before:size-12 3xl:before:size-14  before:size-10 before:top-1/2 before:-translate-y-1/2 before:left-0  before:rounded-full before:transition-all before:duration-500 hover:before:min-w-[100%]">
+                                                <button className="px-4 text-sm text-[white] rounded-full bg-[#0E508A] inline-flex items-center gap-2 font-semibold py-3 border-[#0E508A] border-2 w-max hover:bg-[#0E508A]/80 duration-300">
+                                                    <span>Schedule a consultation</span>
                                                     <span>
-                                                        <img src="./assets/img/arrow.svg" alt="arrow" class="filter invert" />
+                                                        <svg className='size-5' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M928 224H768v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H548v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H328v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H96c-17.7 0-32 14.3-32 32v576c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V256c0-17.7-14.3-32-32-32zm-40 568H136V296h120v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h148v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h148v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h120v496zM416 496H232c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0 136H232c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm308.2-177.4L620.6 598.3l-52.8-73.1c-3-4.2-7.8-6.6-12.9-6.6H500c-6.5 0-10.3 7.4-6.5 12.7l114.1 158.2a15.9 15.9 0 0 0 25.8 0l165-228.7c3.8-5.3 0-12.7-6.5-12.7H737c-5-.1-9.8 2.4-12.8 6.5z"></path></svg>
                                                     </span>
-                                                    Contact HPIT
-                                                </a>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
@@ -48,6 +47,12 @@ function Cta2() {
                 </div>
             </section>
         </div>
+
+
+     
+
+
+
     )
 }
 
