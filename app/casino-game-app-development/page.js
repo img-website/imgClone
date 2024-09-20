744e1a21fffd7ab930d0ae062b79dcce31933d71
Vikram Singh Shekhawat <167164482+vikramimg@users.noreply.github.com>
2024-09-20 17:26:06 +0530

    kabaddi complete casino setup

---
+++ app/casino-game-app-development/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React from "react";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Image from "next/image";
+import Blog from "@/components/blog/Blog";
+import Form from "@/components/contactForm/Form";
+import Cta from "@/components/cta/Cta";
+import Cta2 from "@/components/cta2/Cta2";
+import PortfolioSection from "@/components/portfolio/portfolio";
+import Reward from "@/components/reward/Reward";
+import Gallery from "@/components/gallery/Gallery";
+import Accordion from '@/components/accordion/Accordion';
+import CaseStudy from "@/components/caseStudy/CaseStudy";
+import Testimonial from "@/components/testimonial/Testimonial";
