436dd8ab57e06eb8c0c3885c5a7b7102e44b4a30
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2024-09-23 09:39:55 +0530

    done

---
+++ app/sports-betting-app-development/page.js
@@ -0,0 +1,107 @@
+import Blog from '@/components/blog/Blog'
+import Form from '@/components/contactForm/Form'
+import Footer from '@/components/footer/Footer'
+import Header from '@/components/header/Header'
+import { headers } from 'next/headers'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Cta from '@/components/cta/Cta'
+
+function sportsbettingappdevelopment() {
+  return (
+   <>
+   <Header/>
+     {/* hero */}
+     <div className="relative">
+                <div className="h-[calc(100vh_-_10px)] relative after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-gradient-to-tr  after:via-theme2/80 after:to-theme1/70 after:from-theme1/70 after:backdrop-blur-sm">
+                    <Image src="/img/sports-betting-app-development-1519w.webp" width={'1000'} height={'1000'} alt="" className='h-full w-full object-cover overflow-hidden object-top' />
+                </div>
+                <div className="absolute top-1/2 -translate-y-1/2 flex items-center px-8 sm:px-12 md:px-16 h-full text-white mx-auto w-full">
+                    <div className="w-full">
+                        <div className="max-w-5xl grow-0 text-center mx-auto text-balance">
+                            <div className="font-light text-white max-md:text-2xl/tight max-xl:text-4xl/tight text-6xl/tight tracking-[-0.04em]">
+                                <span className="colorApply3 font-bold text-gray-100 block pb-1  via-indigo-500 from-indigo-100 to-indigo-100">
+                                Sports Betting App Development Company
+                                </span>
+                            </div>
+                            <div className="max-lg:text-sm text-base text-center text-white pt-5 text-balance max-lg:mx-auto">
+                            Supercharge your sports betting business with Twin Power Software — your trusted partner for developing stunning apps. We specialize in transforming betting ideas into powerful sports betting app solutions.
+                            </div>
+                            <div className=" flex justify-center w-full mt-6">
+                                <button className="px-4 text-sm text-[white] rounded-full bg-[#0E508A] inline-flex items-center gap-2 font-semibold py-3 border-[#0E508A] border-2 w-max hover:bg-[#0E508A]/80 duration-300">
+                                    <span>Schedule a consultation</span>
+                                    <span>
+                                        <svg className='size-5' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M928 224H768v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H548v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H328v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H96c-17.7 0-32 14.3-32 32v576c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V256c0-17.7-14.3-32-32-32zm-40 568H136V296h120v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h148v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h148v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h120v496zM416 496H232c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0 136H232c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm308.2-177.4L620.6 598.3l-52.8-73.1c-3-4.2-7.8-6.6-12.9-6.6H500c-6.5 0-10.3 7.4-6.5 12.7l114.1 158.2a15.9 15.9 0 0 0 25.8 0l165-228.7c3.8-5.3 0-12.7-6.5-12.7H737c-5-.1-9.8 2.4-12.8 6.5z"></path></svg>
+                                    </span>
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* hero */}
+
+            <section className="py-24 relative xl:mr-0 lg:mr-5 mr-0">
+        <div className="w-full max-w-7xl px-6 lg:px-8 mx-auto">
+          <div className="w-full justify-start items-center xl:gap-12 gap-10 grid lg:grid-cols-2 grid-cols-1">
+            <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+              <div className="w-full flex-col justify-center items-start gap-8 flex">
+                <div className="flex-col justify-start lg:items-start items-center gap-4 flex">
+                  <div className="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
+                    <p className="max-md:text-2xl/tight pt-16 max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black empty:hidden text-balance">
+                      <span>Innovative Sports Betting 
+                      </span>
+                      <span className="font-bold text-theme2"> App Development Using the Latest Tech</span>
+                    </p>
+                    <p
+                      className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                      Get in the sports betting business with a cutting-edge app that delivers unforgettable experiences for all users.
+
+If you're looking to develop a unique sports betting app, Twin Power Software is the right choice. We blend expertise and innovation to craft high-performing sports betting solutions. With our mastery of the latest technologies, frameworks, and APIs, we simplify complex betting challenges.
+
+Our team of experienced sports betting app developers can create solutions like Bet365, 1xBet, BetMGM, and Parimatch, as well as niche apps for cricket, football, and other sports. As a top sports betting app development company, we provide tech-driven, innovative solutions to bring your vision to life.</p>
+
+                  </div>
+                </div>
+                <div className="w-full flex-col justify-center items-start gap-6 flex">
+                  
+                
+                </div>
+              </div>
+              <button className="px-4 text-sm text-theme1 inline-flex items-center gap-2 font-semibold rounded-full py-3 border-theme1 overflow-hidden border-2 w-max  before:-z-10 relative before:absolute before:w-full before:h-0 before:left-0 before:top-1/2 before:bg-theme1 before:md:hover:h-full before:duration-300 md:hover:text-white duration-300 before:md:hover:top-0">
+                <span>View More</span>
+                <span>
+                  <svg className='size-5' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4z"></path></svg>
+                </span>
+              </button>
+            </div>
+            <div className="w-full lg:justify-start justify-center items-start flex">
+              <div
+                className="sm:w-[500px] w-full aspect-[1/.8] sm:h-auto  sm:bg-gray-100 rounded-3xl sm:border relative">
+                <Image width={1000} height={10000} loading='lazy' quality={90} className="sm:mt-5 sm:ml-5 w-full h-full rounded-3xl object-cover"
+                  src="https://img.freepik.com/premium-photo/dynamic-collage-athletes-various-sports-fencing-boxing-basketball-football-track-tennis-cycling-white-studio-background-team-individual-sports-concept-complete_880763-30980.jpg" alt="about Us image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+            <Cta/>
+
+
+
+
+
+
+
+
+   <Blog/>
+   <Form/>
+   <Footer/>
+   </>
+  )
+}
+
+export default sportsbettingappdevelopment
