52bf95a78c50c29231eb2664fcadf8adb8cd9c4a
Vikram Singh Shekhawat <167164482+vikramimg@users.noreply.github.com>
2024-09-24 18:28:45 +0530

    launch-your-online-ecommerce-store

---
+++ app/launch-your-online-ecommerce-store/page.js
@@ -0,0 +1,144 @@
+"use client"
+import React from "react";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Image from "next/image";
+import Blog from "@/components/blog/Blog";
+import Form from "@/components/contactForm/Form";
+import Cta from "@/components/cta/Cta";
+import Cta2 from "@/components/cta2/Cta2";
+import PortfolioSection from "@/components/portfolio/portfolio";
+import Reward from "@/components/reward/Reward";
+import Gallery from "@/components/gallery/Gallery";
+import Accordion from '@/components/accordion/Accordion';
+import CaseStudy from "@/components/caseStudy/CaseStudy";
+import Testimonial from "@/components/testimonial/Testimonial";
+
+
+
+function launchyouronlineecommercestore() {
+  const accordionItems = [
+      {
+          title: 'What is the Cost of Developing a Logistics App?',
+          content: "The cost to develop a logistics app typically ranges from $12,000 to $35,000. However, it's important to note that this estimate can vary based on several factors, including the app's complexity, size, and specific features."
+      },
+      {
+          title: "What is the Timeline for Developing a Logistics App?",
+          content: "The timeline for developing a logistics app depends on the project's features, complexity, and specific requirements. On average, a logistics app can take between 3 to 9 months to develop, which includes comprehensive testing and quality assurance."
+      },
+      {
+          title: 'What Technologies Are Utilized in Logistics App Development?',
+          content: 'We utilize a range of technologies for cross-platform compatibility, including Java, Swift, and popular frameworks like React Native. Our technology choices are tailored to meet the specific requirements of each project.'
+      },
+      {
+          title: 'Is Third-Party API Integration for Real-Time Tracking and Other Logistics Functions Possible?',
+          content: "We have extensive expertise in integrating third-party APIs for real-time tracking, route optimization, and various logistics services. Our goal is to provide users of your logistics app with a seamless and feature-rich experience."
+      },
+      {
+          title: 'Do You Provide Post-Launch Support and Maintenance for Logistics Apps?What advantages come with outsourcing media and entertainment app development services?',
+          content: 'Yes, we provide comprehensive post-launch support and maintenance services to ensure that your app remains up-to-date, secure, and compliant with evolving industry standards. Our team is always ready to address any issues and implement necessary updates.'
+      },
+  ];
+  return (
+      <>
+              
+              <Header />
+                   {/* hero */}
+
+      <div id="heroColor4" data-color="indigo">
+        <span className="hidden before:from-theme1 before:via-theme1/50 before:to-theme1/80 from-theme1 via-theme1/50 to-theme1/80  text-indigo-700">
+          <span className="via-indigo-500 from-indigo-100 to-indigo-100 *:*:text-indigo-400"></span>
+          <span className="bg-theme1/50 hover:bg-indigo-700"></span>
+        </span>
+        <div className="flex w-full relative bg-slate-50  lg:h-dvh min-h-[650px]">
+          <div className="size-full relative !z-10">
+            <div className="colorApply1 flex size-full items-center rounded-b-2xl relative overflow-hidden shadow-lg before:absolute before:inset-0 before:bg-cover before:bg-right before:rounded-b-2xl before:bg-gradient-to-r before:mix-blend-multiply after:absolute after:rounded-b-2xl after:inset-0 after:bg-repeat   before:from-theme1 before:via-theme1/50 before:to-theme1/80">
+              <span className=" absolute inset-0 bg-cover bg-right opacity-50 rounded-b-2xl bg-gradient-to-r mix-blend-multiply  from-theme1 via-theme1/50 to-theme1/80"></span>
+              <Image
+                className="size-full object-cover object-center absolute inset-0 -z-[1] rounded-b-2xl aspect-[1519/812]"
+                width="1519"
+                height="812"
+                loading="lazy"
+                src="/img/launch-online-ecommerce-marketplace.webp"
+                alt="image.
+"
+              />
+
+              <div className="flex mx-auto max-lg:flex-col max-lg:gap-y-10 items-center 2xl:max-w-7xl xl:max-w-6xl lg:max-w-5xl md:max-w-3xl sm:max-w-3xl max-w-2xl px-6 lg:px-8 w-full relative lg:pt-40 sm:pt-32 pt-28 pb-12 overflow-hidden z-10 justify-between">
+                <div className="xl:w-7/12 lg:w-1/2 grow-0 max-lg:text-center">
+                  <div className="font-light text-white max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em]">
+                    <span className="colorApply3 font-bold text-gray-100 block pb-1  via-indigo-500 from-indigo-100 to-indigo-100">
+                    Kickstart Your Ecommerce Marketplace Today!
+
+                    </span>
+                  </div>
+                  <div className="max-lg:text-sm text-base text-white/80 pt-5 text-pretty w-10/12 max-lg:mx-auto">
+                  Eager to become the next big name in the ecommerce space? Create an impressive ecommerce marketplace and capture the hearts of online shoppers. Choose IMG Global Infotech as your trusted partner!
+                  </div>
+                </div>
+                <div className="xl:w-1/3 lg:w-1/2 max-lg:mx-auto">
+                  <div className="colorApply4 w-full px-5 md:py-10 py-8 md:rounded-2xl rounded-xl  shadow-xl bg-white backdrop-blur-lg relative gap-6 flex flex-col before:absolute before:inset-4 md:hover:before:inset-1 before:duration-200 before:ease-in before:bg-white/5 before:backdrop-blur-2xl before:rounded-xl before: before:z-0 before:m-auto *:w-full *:z-10 ">
+                    <div>
+                      <div className="colorApply5 size-16 rounded-full mx-auto shadow-md shadow-white/5 bg-theme1  text-white">
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          stroke-width="1.5"
+                          viewBox="0 0 24 24"
+                          aria-hidden="true"
+                          className="size-full"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="lg:text-xl md:text-lg text-base text-theme1 text-center text-balance font-semibold">
+                      Get Top-Notch, Innovative Mobile App Solutions from Us.
+                    </div>
+                    <div className=" flex justify-center w-full">
+                      <button className="px-4 text-sm text-[white] rounded-full bg-[#0E508A] inline-flex items-center gap-2 font-semibold py-3 border-[#0E508A] border-2 w-max hover:bg-[#0E508A]/80 duration-300">
+                        <span>Schedule a consultation</span>
+                        <span>
+                          <svg
+                            className="size-5"
+                            stroke="currentColor"
+                            fill="currentColor"
+                            stroke-width="0"
+                            viewBox="0 0 1024 1024"
+                            height="200px"
+                            width="200px"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M928 224H768v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H548v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H328v-56c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v56H96c-17.7 0-32 14.3-32 32v576c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V256c0-17.7-14.3-32-32-32zm-40 568H136V296h120v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h148v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h148v56c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-56h120v496zM416 496H232c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0 136H232c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h184c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm308.2-177.4L620.6 598.3l-52.8-73.1c-3-4.2-7.8-6.6-12.9-6.6H500c-6.5 0-10.3 7.4-6.5 12.7l114.1 158.2a15.9 15.9 0 0 0 25.8 0l165-228.7c3.8-5.3 0-12.7-6.5-12.7H737c-5-.1-9.8 2.4-12.8 6.5z"></path>
+                          </svg>
+                        </span>
+                      </button>
+                    </div>
+
+                    <div className="md:text-sm text-xs text-theme1 text-center text-balance">
+                      Complete the form below with your project specifications.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+              <Blog />
+            <Form />
+            <Footer />
+
+</>
+    )
+}
+
+export default launchyouronlineecommercestore;
